2113a4406849ef39c790d2b9d605bd90ab9a84ef
StakNine <staknine@protonmail.com>
2021-11-12 21:24:47 -0800

    Add error

--- lib/hello_app_signal_web/controllers/page_controller.ex
+++ lib/hello_app_signal_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule HelloAppSignalWeb.PageController do
   use HelloAppSignalWeb, :controller
 
   def index(conn, _params) do
+    raise ArgumentError, message: "We are testing error reporting with AppSignal"
     render(conn, "index.html")
   end
 end
